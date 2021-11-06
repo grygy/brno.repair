8b85c5124c989f3ee4e001bf25acee4f60c8eebf
Miroslav Bezak <mbezak@redhat.com>
2021-11-06 18:45:05 +0100

    Fix missing Adam's colon

---
+++ src/utils/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXF3hRiE3zw7IMnaKRNr9q2Odeczgsz-c",
+  authDomain: "brnodotrepair.firebaseapp.com",
+  projectId: "brnodotrepair",
+  storageBucket: "brnodotrepair.appspot.com",
+  messagingSenderId: "203582460583",
+  appId: "1:203582460583:web:95e0064fa6bdfd717f8182"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
