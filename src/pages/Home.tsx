68fe34529ea8d70bea54f520f328a381346041b4
Miroslav Bezak <mbezak@redhat.com>
2021-11-22 17:38:26 +0100

    Implement ProblemPreview

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { Box, CircularProgress, Typography } from '@mui/material';
 
 import { Problem, getLastProblems } from '../utils/firebase';
+import ProblemPreview from '../components/ProblemPreview';
 
 const Home = () => {
 	const [problems, setProblems] = useState<Problem[]>([]);
 	const [loading, setLoading] = useState(true);
 	useEffect(() => {
 		(async () => {
-			const res = await getLastProblems(10);
+			const res = await getLastProblems(3);
 			setProblems(res);
 			setLoading(false);
 		})();
@@ -20,11 +21,14 @@ const Home = () => {
 
 	return (
 		<>
-			{problems.map(problem => (
-				<Box key={problem.created.toString()}>
-					<Typography>{problem.title}</Typography>
-				</Box>
-			))}
+			<Typography mt={2} variant="h4">
+				3 najnovsie problemy
+			</Typography>
+			<Box sx={{ display: 'flex', gap: 2 }}>
+				{problems.map(problem => (
+					<ProblemPreview key={problem.created.toString()} problem={problem} />
+				))}
+			</Box>
 		</>
 	);
 };
